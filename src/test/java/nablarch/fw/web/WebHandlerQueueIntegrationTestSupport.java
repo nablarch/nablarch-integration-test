ec7a79328fd3ba54377fdce2d84aafe551f0cf7c
siosio <siosio12@gmail.com>
2017-11-06 14:19:45 +0900

    to maven project

--- src/test/java/nablarch/fw/web/WebHandlerQueueIntegrationTestSupport.java
+++ src/test/java/nablarch/fw/web/WebHandlerQueueIntegrationTestSupport.java
@@ -1,27 +1,35 @@
 package nablarch.fw.web;
 
-import com.google.api.client.http.*;
-import com.google.api.client.http.HttpRequest;
-import com.google.api.client.http.javanet.NetHttpTransport;
-import nablarch.fw.web.upload.CustomMultipartContent;
-import nablarch.fw.web.upload.MockMultipartParser;
-import nablarch.test.support.log.app.OnMemoryLogWriter;
-import org.jboss.arquillian.container.test.api.RunAsClient;
-import org.jboss.arquillian.test.api.ArquillianResource;
-import org.junit.Before;
-import org.junit.Rule;
-import org.junit.Test;
-import org.junit.rules.TemporaryFolder;
+import static junit.framework.TestCase.fail;
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.assertThat;
 
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.net.URL;
-import java.util.List;
 
-import static junit.framework.TestCase.fail;
-import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.assertThat;
+import com.google.api.client.http.ByteArrayContent;
+import com.google.api.client.http.FileContent;
+import com.google.api.client.http.GenericUrl;
+import com.google.api.client.http.HttpHeaders;
+import com.google.api.client.http.HttpMediaType;
+import com.google.api.client.http.HttpRequest;
+import com.google.api.client.http.HttpResponseException;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.http.MultipartContent;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import org.jboss.arquillian.container.test.api.RunAsClient;
+import org.jboss.arquillian.test.api.ArquillianResource;
+
+import nablarch.fw.web.upload.CustomMultipartContent;
+import nablarch.fw.web.upload.MockMultipartParser;
+import nablarch.test.support.log.app.OnMemoryLogWriter;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
 
 /**
  * ウェブアプリケーションにおける標準ハンドラ構成での結合テストをサポートするクラス。
@@ -41,7 +49,7 @@ public abstract class WebHandlerQueueIntegrationTestSupport {
     @Before
     public void setUp() throws Exception {
         // アップロードファイルが作成されていれば削除する。
-        File file = new File(System.getProperty("java.io.tmpdir"), "uploadFile");
+        final File file = new File(System.getProperty("java.io.tmpdir"), "uploadFile");
         if (file.exists()) {
             file.delete();
         }
@@ -57,12 +65,12 @@ public abstract class WebHandlerQueueIntegrationTestSupport {
     @Test
     @RunAsClient
     public void testMultipart_success() throws Exception {
-        File file = folder.newFile("multipart.txt");
+        final File file = folder.newFile("multipart.txt");
 
-        com.google.api.client.http.HttpRequest request = httpTransport.createRequestFactory()
-                .buildPostRequest(new GenericUrl(new URL(baseUrl, "action/MultipartAction/Upload")), createMultipartContent(file));
+        final com.google.api.client.http.HttpRequest request = httpTransport.createRequestFactory()
+                                                                            .buildPostRequest(new GenericUrl(new URL(baseUrl, "action/MultipartAction/Upload")), createMultipartContent(file));
 
-        com.google.api.client.http.HttpResponse response = request.execute();
+        final com.google.api.client.http.HttpResponse response = request.execute();
 
         assertThat(response.getStatusCode(), is(200));
         assertThat(response.parseAsString(), is("SUCCESS"));
@@ -78,13 +86,13 @@ public abstract class WebHandlerQueueIntegrationTestSupport {
     @Test
     @RunAsClient
     public void testMultipart_nothing_boundary() throws Exception {
-        File file = folder.newFile("multipart.txt");
+        final File file = folder.newFile("multipart.txt");
         try {
-            com.google.api.client.http.HttpRequest request = httpTransport.createRequestFactory()
-                    .buildPostRequest(new GenericUrl(new URL(baseUrl, "action/MultipartAction/Upload")), createCustomMultipartContent(file));
+            final com.google.api.client.http.HttpRequest request = httpTransport.createRequestFactory()
+                                                                                .buildPostRequest(new GenericUrl(new URL(baseUrl, "action/MultipartAction/Upload")), createCustomMultipartContent(file));
             request.execute();
             fail("boundaryが設定されていないため、BAD REQUEST(400)が送出される。");
-        } catch (HttpResponseException e) {
+        } catch (final HttpResponseException e) {
             assertThat(e.getStatusCode(), is(400));
         }
     }
@@ -97,13 +105,13 @@ public abstract class WebHandlerQueueIntegrationTestSupport {
     @Test
     @RunAsClient
     public void testMultipart_too_large() throws Exception {
-        File file = createLargeFile("multipart.txt");
+        final File file = createLargeFile("multipart.txt");
         try {
-            com.google.api.client.http.HttpRequest request = httpTransport.createRequestFactory()
-                    .buildPostRequest(new GenericUrl(new URL(baseUrl, "action/MultipartAction/Upload")), createMultipartContent(file));
+            final com.google.api.client.http.HttpRequest request = httpTransport.createRequestFactory()
+                                                                                .buildPostRequest(new GenericUrl(new URL(baseUrl, "action/MultipartAction/Upload")), createMultipartContent(file));
             request.execute();
             fail("アップロードファイルサイズが上限を超えたため、REQUEST ENTITY TOO LARGE(413)が送出される。");
-        } catch (HttpResponseException e) {
+        } catch (final HttpResponseException e) {
             assertThat(e.getStatusCode(), is(413));
         }
     }
@@ -120,14 +128,14 @@ public abstract class WebHandlerQueueIntegrationTestSupport {
         // 一時ファイルの作成時にエラーが発生するようにモックを定義。
         new MockMultipartParser();
 
-        File file = folder.newFile("multipart.txt");
+        final File file = folder.newFile("multipart.txt");
         try {
             final HttpRequest request = httpTransport.createRequestFactory()
                     .buildPostRequest(new GenericUrl(new URL(baseUrl, "action/MultipartAction/Upload")), createMultipartContent(file));
 
             request.execute();
             fail("一時ファイルの作成に失敗したため、INTERNAL SERVER ERROR(500)が送出される。");
-        } catch (HttpResponseException e) {
+        } catch (final HttpResponseException e) {
             assertThat(e.getStatusCode(), is(500));
             Thread.sleep(100); // ログ出力される前にassertされるのを防ぐため
             OnMemoryLogWriter.assertLogContains(
@@ -144,8 +152,8 @@ public abstract class WebHandlerQueueIntegrationTestSupport {
      * @return {@link MultipartContent}
      * @throws IOException 入出力例外
      */
-    protected MultipartContent createMultipartContent(File file) throws IOException {
-        MultipartContent content = new MultipartContent();
+    protected MultipartContent createMultipartContent(final File file) throws IOException {
+        final MultipartContent content = new MultipartContent();
         content.setMediaType(new HttpMediaType("multipart/form-data")
                 .setParameter("boundary", "__END_OF_PART__"));
 
@@ -167,11 +175,11 @@ public abstract class WebHandlerQueueIntegrationTestSupport {
      * @return {@link CustomMultipartContent}
      * @throws IOException 入出力例外
      */
-    protected CustomMultipartContent createCustomMultipartContent(File file) throws IOException {
-        CustomMultipartContent content = new CustomMultipartContent();
+    protected CustomMultipartContent createCustomMultipartContent(final File file) throws IOException {
+        final CustomMultipartContent content = new CustomMultipartContent();
         content.setMediaType(new HttpMediaType("multipart/form-data"));
 
-        CustomMultipartContent.Part part = new CustomMultipartContent.Part(new FileContent("application/octet-stream", file));
+        final CustomMultipartContent.Part part = new CustomMultipartContent.Part(new FileContent("application/octet-stream", file));
         part.setHeaders(new HttpHeaders().set("Content-Disposition", String.format("form-data; name=\"uploadFile\"; filename=\"%s\"", file.getName())));
         return content.addPart(part);
     }
@@ -183,9 +191,9 @@ public abstract class WebHandlerQueueIntegrationTestSupport {
      * @return ファイル
      * @throws IOException 入出力例外
      */
-    protected File createLargeFile(String fileName) throws IOException {
-        File file = folder.newFile(fileName);
-        FileWriter filewriter = new FileWriter(file);
+    protected File createLargeFile(final String fileName) throws IOException {
+        final File file = folder.newFile(fileName);
+        final FileWriter filewriter = new FileWriter(file);
         for (int i = 0; i < 10000; i++) {
             filewriter.write("test");
         }
